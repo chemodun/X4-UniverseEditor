ba3f92a302f37988302f6aa211e03d378f4815a2
Chem O'Dun <chemodun@gmail.com>
2025-01-19 15:53:18 +0200

    feat: enhance MainWindow with configuration options and data handling

--- src/ChemGateBuilder/MainWindow.xaml.cs
+++ src/ChemGateBuilder/MainWindow.xaml.cs
@@ -1,23 +1,191 @@
-﻿using System.Text;
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace ChemGateBuilder;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace ChemGateBuilder
 {
-    public MainWindow()
+    public class AppConfig
     {
-        InitializeComponent();
+        public EditConfig Edit { get; set; } = new EditConfig();
+        public DataConfig Data { get; set; } = new DataConfig();
+        public LoggingConfig Logging { get; set; } = new LoggingConfig();
+    }
+
+    public class EditConfig
+    {
+        public bool GatesActiveByDefault { get; set; } = true;
+        public int GatesMinimalDistanceBetween { get; set; } = 10;
+    }
+
+    public class DataConfig
+    {
+        public string X4DataExtractedPath { get; set; } = ".";
+    }
+
+    public class LoggingConfig
+    {
+        public string LogLevel { get; set; } = "WARNING";
+        public bool LogToFile { get; set; } = false;
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string _configFileName;
+        private string _x4DataFolder;
+        private bool _gatesActiveByDefault;
+        private int _gatesMinimalDistanceBetween;
+        private string _logLevel;
+        private bool _logToFile;
+
+        public string X4DataFolder
+        {
+            get => _x4DataFolder;
+            set
+            {
+                _x4DataFolder = value;
+                OnPropertyChanged(nameof(X4DataFolder));
+                OnPropertyChanged(nameof(X4DataFolderStatus));
+                SaveConfiguration();
+            }
+        }
+
+        public string X4DataFolderStatus
+        {
+            get
+            {
+                if (Directory.Exists(X4DataFolder))
+                {
+                    return System.IO.Path.GetFullPath(X4DataFolder);
+                }
+                else
+                {
+                    return $"Error: Folder does not exist ({X4DataFolder})";
+                }
+            }
+        }
+
+        public bool GatesActiveByDefault
+        {
+            get => _gatesActiveByDefault;
+            set
+            {
+                _gatesActiveByDefault = value;
+                OnPropertyChanged(nameof(GatesActiveByDefault));
+                SaveConfiguration();
+            }
+        }
+
+        public int GatesMinimalDistanceBetween
+        {
+            get => _gatesMinimalDistanceBetween;
+            set
+            {
+                _gatesMinimalDistanceBetween = value;
+                OnPropertyChanged(nameof(GatesMinimalDistanceBetween));
+                SaveConfiguration();
+            }
+        }
+
+        public string LogLevel
+        {
+            get => _logLevel;
+            set
+            {
+                _logLevel = value;
+                OnPropertyChanged(nameof(LogLevel));
+                SaveConfiguration();
+            }
+        }
+
+        public bool LogToFile
+        {
+            get => _logToFile;
+            set
+            {
+                _logToFile = value;
+                OnPropertyChanged(nameof(LogToFile));
+                SaveConfiguration();
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            _configFileName = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.json";
+            LoadConfiguration();
+        }
+
+        private void LoadConfiguration()
+        {
+            if (File.Exists(_configFileName))
+            {
+                var jsonString = File.ReadAllText(_configFileName);
+                var config = JsonSerializer.Deserialize<AppConfig>(jsonString);
+
+                if (config != null)
+                {
+                    X4DataFolder = config.Data.X4DataExtractedPath;
+                    GatesActiveByDefault = config.Edit.GatesActiveByDefault;
+                    GatesMinimalDistanceBetween = config.Edit.GatesMinimalDistanceBetween;
+                    LogLevel = config.Logging.LogLevel;
+                    LogToFile = config.Logging.LogToFile;
+                }
+            }
+            else
+            {
+                // Set default values if the config file does not exist
+                X4DataFolder = ".";
+                GatesActiveByDefault = true;
+                GatesMinimalDistanceBetween = 10;
+                LogLevel = "Warning";
+                LogToFile = false;
+            }
+        }
+
+        private void SaveConfiguration()
+        {
+            var config = new AppConfig
+            {
+                Data = new DataConfig { X4DataExtractedPath = X4DataFolder },
+                Edit = new EditConfig
+                {
+                    GatesActiveByDefault = GatesActiveByDefault,
+                    GatesMinimalDistanceBetween = GatesMinimalDistanceBetween
+                },
+                Logging = new LoggingConfig
+                {
+                    LogLevel = LogLevel,
+                    LogToFile = LogToFile
+                }
+            };
+
+            var jsonString = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(_configFileName, jsonString);
+        }
+
+        private void SelectX4DataFolder_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                X4DataFolder = dialog.SelectedPath;
+            }
+        }
+
+        // Removed the SelectionChanged event handler as it's no longer needed
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
