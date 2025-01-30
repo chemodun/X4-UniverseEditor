00252117eb1fa8d2090233ff8727339eece19777
Chem O'Dun <chemodun@gmail.com>
2025-01-30 19:36:40 +0200

    Refactor(ClusterMap): Refactor to use ClusterMapCluster and ClusterMapSector classes. Avoid to recrate all staff on any change

--- src/ChemGateBuilder/ClusterMapWindow.xaml.cs
+++ src/ChemGateBuilder/ClusterMapWindow.xaml.cs
@@ -15,7 +15,7 @@ namespace ChemGateBuilder
     {
         // Constants for hexagon dimensions and scaling
         private const double HexagonWidthDefault = 100;      // Width of the hexagon in pixels
-        private static readonly double HexagonSizesRelation = Math.Sqrt(3)/2;    // Height of the hexagon in pixels (Width * sqrt(3)/2)
+        public static readonly double HexagonSizesRelation = Math.Sqrt(3)/2;    // Height of the hexagon in pixels (Width * sqrt(3)/2)
         private static readonly double HexagonHeightDefault = HexagonWidthDefault * HexagonSizesRelation;    // Height of the hexagon in pixels (Width * sqrt(3)/2)
         private double _hexagonWidth = HexagonWidthDefault;      // Width of the hexagon in pixels
         public double HexagonWidth {
@@ -25,7 +25,7 @@ namespace ChemGateBuilder
                 _hexagonHeight = value * HexagonSizesRelation;
                 OnPropertyChanged(nameof(HexagonWidth));
                 OnPropertyChanged(nameof(HexagonHeight));
-                DrawClusterMap();
+                UpdateMap();
             }
         }      // Width of the hexagon in pixel
 
@@ -51,16 +51,23 @@ namespace ChemGateBuilder
             }
         }
 
-        private const double ColumnWidth = 15000000;  // 15,000,000 units for horizontal (X) axis
-        private const double RowHeight = 17320000;    // 17,320,000 units for vertical (Z) axis
+        private const double ColumnWidth = 15000000;  // 15,000,000 meters for horizontal (X) axis
+        private const double RowHeight = 17320000;    // 17,320,000 meters for vertical (Z) axis
 
-        private double ScaleFactor = 0.001;   // Scaling factor to convert units to pixels
-        private readonly Dictionary<string, Dictionary<string, string>> mapDict = [];
+        public double ScaleFactor = 0.001;   // Scaling factor to convert units to pixels
+        private double _canvasWidthBase = 0;
+        private double _canvasHeightBase = 0;
+        private readonly Dictionary<string, Dictionary<string, Cluster>> mapDict = [];
         private readonly List<double> AxisZ = [];
         private readonly List<int> AxisX = [];
 
         // Reference to the main window's size (assumed to be passed or accessible)
         private readonly MainWindow MainWindowReference;
+        private readonly Galaxy? Galaxy;
+
+        private readonly List<ClusterMapCluster> _clusters = [];
+
+        public Sector? SelectedSector = null;
 
         public ClusterMapWindow(MainWindow mainWindow)
         {
@@ -68,6 +75,7 @@ namespace ChemGateBuilder
             DataContext = this;
             Owner = mainWindow;
             MainWindowReference = mainWindow;
+            Galaxy = MainWindowReference.Galaxy;
 
             // Set window size to 90% of the main window
             Width = mainWindow.ActualWidth * 0.9;
@@ -81,7 +89,7 @@ namespace ChemGateBuilder
                 Log.Error("Cluster map is not prepared.");
                 return;
             }
-            DrawClusterMap();
+            UpdateMap();
         }
 
         /// <summary>
@@ -90,32 +98,25 @@ namespace ChemGateBuilder
         /// <returns>True if the map was prepared successfully; otherwise, false.</returns>
         public bool PrepareClusterMap()
         {
-            if (MainWindowReference.Galaxy == null)
+            if (Galaxy == null)
             {
                 Log.Error("Galaxy data is not loaded.");
                 return false;
             }
 
             // Determine the rowId and columnId for each cluster and populate the dictionary
-            foreach (var cluster in MainWindowReference.Galaxy.Clusters)
+            foreach (var cluster in Galaxy.Clusters)
             {
                 string columnIdStr = $"{(int)Math.Floor(cluster.Position.X / ColumnWidth)}";
                 string rowIdStr = $"{cluster.Position.Z / RowHeight:F1}";
 
-                if (!mapDict.TryGetValue(rowIdStr, out Dictionary<string, string>? value))
+                if (!mapDict.TryGetValue(rowIdStr, out Dictionary<string, Cluster>? value))
                 {
                     value = [];
                     mapDict[rowIdStr] = value;
                 }
 
-                if (cluster.Sectors.Count > 1)
-                {
-                    value[columnIdStr] = cluster.Name;
-                }
-                else
-                {
-                    value[columnIdStr] = $"{cluster.Sectors[0].Name} ({cluster.Name})";
-                }
+                value[columnIdStr] = cluster;
 
                 if (!AxisZ.Contains(double.Parse(rowIdStr)))
                 {
@@ -134,10 +135,7 @@ namespace ChemGateBuilder
             return true;
         }
 
-        /// <summary>
-        /// Draws the cluster map using hexagons on the Canvas.
-        /// </summary>
-        private void DrawClusterMap()
+        private void CreateMap()
         {
             if (mapDict.Count == 0)
             {
@@ -152,35 +150,44 @@ namespace ChemGateBuilder
             double maxRow = AxisZ.First();
             int minCol = AxisX.First();
 
-            // Calculate canvas size with scaling
-            double canvasWidth = (AxisX.Count * 0.75 + 0.25) * HexagonWidth * ScaleFactor;
-            double canvasHeight = (AxisZ.Count + 1) * 0.5 * HexagonHeight * ScaleFactor;
+            _canvasWidthBase = AxisX.Count * 0.75 + 0.25;
+            _canvasHeightBase = (AxisZ.Count + 1) * 0.5;
 
-            // Set Canvas size
-            ClusterCanvas.Width = canvasWidth;
-            ClusterCanvas.Height = canvasHeight;
+            ClusterCanvas.Width = _canvasWidthBase * HexagonWidth * ScaleFactor;
+            ClusterCanvas.Height = _canvasHeightBase * HexagonHeight * ScaleFactor;
 
             ClusterCanvas.Children.Clear();
-
             foreach (var Z in AxisZ)
             {
-                Dictionary<string, string> row = mapDict[Z.ToString("F1")];
+                Dictionary<string, Cluster> row = mapDict[Z.ToString("F1")];
                 foreach (var X in AxisX)
                 {
                     if (!row.ContainsKey(X.ToString()) )
                     {
                         continue;
                     }
-                    string clusterName = row[X.ToString()];
-                    // Calculate pixel positions
-                    double x = (X - minCol) * HexagonWidth * 3 / 4  * ScaleFactor;
-                    double y = (maxRow - Z) * HexagonHeight * ScaleFactor;
+                    Cluster cluster = row[X.ToString()];
+                    ClusterMapCluster clusterMapCluster = new(this, 0.75 * (X - minCol), maxRow - Z, ClusterCanvas, cluster);
+                    clusterMapCluster.Create();
+                    _clusters.Add(clusterMapCluster);
+                }
+            }
 
-                    // Create hexagon with centered text
-                    Grid hexGrid = CreateHexagon(x, y, HexagonWidth * ScaleFactor, HexagonHeight * ScaleFactor, clusterName);
+        }
 
-                    // Add hexagon grid to Canvas
-                    ClusterCanvas.Children.Add(hexGrid);
+        private void UpdateMap()
+        {
+            if (_clusters.Count == 0)
+            {
+                CreateMap();
+            }
+            else
+            {
+                ClusterCanvas.Width = _canvasWidthBase * HexagonWidth * ScaleFactor;
+                ClusterCanvas.Height = _canvasHeightBase * HexagonHeight * ScaleFactor;
+                foreach (ClusterMapCluster cluster in _clusters)
+                {
+                    cluster.Update();
                 }
             }
         }
@@ -203,75 +210,7 @@ namespace ChemGateBuilder
                     HexagonWidthMaximal = height / HexagonSizesRelation / ScaleFactor;
                 }
             }
-            DrawClusterMap();
-        }
-
-        /// <summary>
-        /// Creates a hexagon Polygon with centered TextBlock wrapped in a Grid.
-        /// </summary>
-        /// <param name="x">Top-left X position on Canvas.</param>
-        /// <param name="y">Top-left Y position on Canvas.</param>
-        /// <param name="width">Width of the hexagon.</param>
-        /// <param name="height">Height of the hexagon.</param>
-        /// <param name="text">Text to display at the center.</param>
-        /// <returns>Grid containing the hexagon and the TextBlock.</returns>
-        private static Grid CreateHexagon(double x, double y, double width, double height, string text)
-        {
-            // Create the hexagon polygon
-            Polygon hex = new()
-            {
-                Stroke = Brushes.Black,
-                StrokeThickness = 1,
-                Fill = Brushes.LightGray,
-                Tag = text
-            };
-
-            // Define the points of the hexagon relative to (0,0)
-            PointCollection points =
-            [
-                new Point(width * 0.25, 0),
-                new Point(width * 0.75, 0),
-                new Point(width, height * 0.5),
-                new Point(width * 0.75, height),
-                new Point(width * 0.25, height),
-                new Point(0, height * 0.5)
-            ];
-
-            hex.Points = points;
-
-            // Create TextBlock
-            TextBlock tb = new()
-            {
-                Text = text,
-                Foreground = Brushes.Black,
-                FontSize = Math.Min(height * 0.2, 8), // Initial proportional font size
-                TextAlignment = TextAlignment.Center,
-                HorizontalAlignment = HorizontalAlignment.Center,
-                VerticalAlignment = VerticalAlignment.Center,
-                TextWrapping = TextWrapping.Wrap
-            };
-
-            // Create Grid to contain Hexagon and TextBlock
-            Grid grid = new()
-            {
-                Width = width,
-                Height = height
-            };
-
-            grid.Children.Add(hex);
-            grid.Children.Add(tb);
-
-            // Position the Grid on the Canvas
-            Canvas.SetLeft(grid, x);
-            Canvas.SetTop(grid, y);
-
-            // Add click event to hexagon
-            hex.MouseLeftButtonUp += (s, e) =>
-            {
-                MessageBox.Show($"Cluster: {text}", "Cluster Information", MessageBoxButton.OK, MessageBoxImage.Information);
-            };
-
-            return grid;
+            UpdateMap();
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
@@ -279,6 +218,211 @@ namespace ChemGateBuilder
         protected virtual void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    class ClusterMapCluster(ClusterMapWindow map, double x, double y, Canvas canvas, Cluster cluster) : INotifyPropertyChanged
+    {
+        protected Cluster? Cluster = cluster;
+
+        private readonly double _x = x;
+        protected double X {
+            get {
+                if (Map != null)
+                {
+                    return _x * Map.HexagonWidth * Map.ScaleFactor;
+                }
+                return 0;
+            }
+        }
+
+        private readonly double _y = y;
+        protected double Y {
+            get {
+                if (Map != null)
+                {
+                    return _y * Map.HexagonHeight * Map.ScaleFactor;
+                }
+                return 0;
+            }
+        }
+        protected double Width {
+            get {
+                if (Map != null)
+                {
+                    return Map.HexagonWidth * Map.ScaleFactor;
+                }
+                return 0;
+            }
+        }
+        protected double Height {
+            get {
+                if (Map != null)
+                {
+                    return  Map.HexagonHeight * Map.ScaleFactor;
+                }
+                return 0;
+            }
+        }
+        protected ClusterMapWindow? Map = map;
+        protected Canvas? Canvas = canvas;
+        protected PointCollection Points = [];
+        protected Polygon? Hexagon = null;
+
+        private readonly List<ClusterMapSector> _sectors = [];
+
+        public virtual void Create()
+        {
+            if (Map == null ||Cluster == null || Canvas == null)
+            {
+                return;
+            }
+            UpdatePoints();
+            Log.Debug($"Creating cluster {Cluster.Name} at ({X}, {Y}) ({x}, {y}) with Points {string.Join(", ", Points.Select(p => $"({p.X}, {p.Y})"))})");
+            Hexagon = new()
+            {
+                Stroke = Brushes.Black,
+                StrokeThickness = 1,
+                Fill = Brushes.LightGray,
+                Tag = Cluster.Name,
+                DataContext = Cluster,
+                Points = Points
+            };
+            // Position the Hexagon on the Canvas
+            Canvas.SetLeft(Hexagon, X);
+            Canvas.SetTop(Hexagon, Y);
+            Canvas.Children.Add(Hexagon);
+            if (Cluster.Sectors.Count == 1)
+            {
+                Sector sector = Cluster.Sectors[0];
+                ClusterMapSector clusterMapSector = new(Map, _x, _y, Canvas, Cluster, sector);
+                clusterMapSector.Create();
+                _sectors.Add(clusterMapSector);
+            }
+        }
+
+        public virtual void Update()
+        {
+            if (Cluster == null || Canvas == null || Hexagon == null)
+            {
+                return;
+            }
+            UpdatePoints();
+            Hexagon.Points = Points;
+            // Position the Hexagon on the Canvas
+            Canvas.SetLeft(Hexagon, X);
+            Canvas.SetTop(Hexagon, Y);
+            foreach (ClusterMapSector sector in _sectors)
+            {
+                sector.Update();
+            }
+        }
+
+        protected void UpdatePoints()
+        {
+            Points.Clear();
+            Points.Add(new Point(Width * 0.25, 0));
+            Points.Add(new Point(Width * 0.75, 0));
+            Points.Add(new Point(Width, Height * 0.5));
+            Points.Add(new Point(Width * 0.75, Height));
+            Points.Add(new Point(Width * 0.25, Height));
+            Points.Add(new Point(0, Height * 0.5));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    class ClusterMapSector(ClusterMapWindow map, double x, double y, Canvas canvas, Cluster cluster, Sector sector) : ClusterMapCluster(map, x, y, canvas, cluster)
+    {
+        protected Sector? Sector = sector;
+        protected Grid? Grid = null;
+        protected TextBox? TextBox = null;
+
+        public override void Create()
+        {
+            if (Cluster == null || Sector == null || Canvas == null)
+            {
+                return;
+            }
+            UpdatePoints();
+            Hexagon = new()
+            {
+                Stroke = Brushes.Black,
+                StrokeThickness = 1,
+                Fill = Brushes.LightGray,
+                Tag = Sector.Name,
+                DataContext = Sector,
+                Points = Points
+            };
+            Grid = new()
+            {
+                Width = Width,
+                Height = Height,
+                DataContext = Sector
+            };
+
+            Grid.Children.Add(Hexagon);
+            // Create TextBox
+            TextBox = new()
+            {
+                Text = Sector.Name,
+                Foreground = Brushes.Black,
+                FontSize = Math.Min(Height * 0.1, 22), // Initial proportional font size
+                TextAlignment = TextAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Top,
+                TextWrapping = TextWrapping.Wrap,
+                BorderThickness = new Thickness(1),
+                IsReadOnly = true
+            };
+            Grid.Children.Add(TextBox);
+            // Position the Hexagon on the Canvas
+            Canvas.SetLeft(Grid, X);
+            Canvas.SetTop(Grid, Y);
+            Canvas.Children.Add(Grid);
+            Grid.MouseLeftButtonUp += (s, e) =>
+            {
+                Sector? sector = null;
+                if (s is Polygon polygon && polygon.DataContext is Sector _sectorFromPolygon)
+                {
+                    sector = _sectorFromPolygon;
+                }
+                else if (s is Grid grid && grid.DataContext is Sector _sectorFromGrid)
+                {
+                    sector = _sectorFromGrid;
+                }
+                if (sector != null)
+                {
+                    MessageBoxResult result = MessageBox.Show($"Do you want to select Sector: {sector.Name}", "Select Sector", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                    if (result == MessageBoxResult.Yes && Map != null)
+                    {
+                        Map.SelectedSector = sector;
+                        Map.Close();
+                    }
+                }
+            };
+        }
+
+        public override void Update()
+        {
+            if (Cluster == null || Sector == null || Canvas == null || Hexagon == null || Grid == null || TextBox == null)
+            {
+                return;
+            }
+            UpdatePoints();
+            Hexagon.Points = Points;
+            // Update TextBox
+            TextBox.FontSize = Math.Min(Height * 0.1, 22);
+            Grid.Width = Width;
+            Grid.Height = Height;
+            // Position the Hexagon on the Canvas
+            Canvas.SetLeft(Grid, X);
+            Canvas.SetTop(Grid, Y);
         }
     }
 }
