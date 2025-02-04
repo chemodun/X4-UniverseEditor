418a5e5d1b86ee2f10cd4679a809b02698e67f52
Chem O'Dun <chemodun@gmail.com>
2025-02-04 23:33:43 +0200

    fix(AboutWindow): add components list to About dialog

--- src/ChemGateBuilder/AboutWindow.xaml.cs
+++ src/ChemGateBuilder/AboutWindow.xaml.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Navigation;
@@ -10,7 +12,19 @@ namespace ChemGateBuilder
     {
         public string Version { get; set; }
         public string Copyright { get; set; }
+        public List<string> Components { get; set; }
 
+        private readonly string[] ComponentsToExclude =
+        [
+            "System.",
+            "Microsoft.",
+            "WindowsBase",
+            "PresentationCore",
+            "PresentationFramework",
+            "Windows",
+            "UIAutomation",
+            "DirectWriteForwarder",
+        ];
         public AboutWindow()
         {
             InitializeComponent();
@@ -18,20 +32,50 @@ namespace ChemGateBuilder
 
             // Get the version information from the assembly
             var version = Assembly.GetExecutingAssembly().GetName().Version;
-
             Version = $"Version {version?.ToString()}";
+
+            // Get the copyright information from the assembly
             Copyright = $"{Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyCopyrightAttribute>()?.Copyright ?? ""}";
+
+            // Get the list of components and their versions
+            Components = GetReferencedAssemblies();
+        }
+
+        private List<string> GetReferencedAssemblies()
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var componentList = new List<string>();
+
+            foreach (var assembly in assemblies)
+            {
+                var name = assembly.GetName();
+                if (name == null || name.Name == null)
+                {
+                    continue;
+                }
+                if (ComponentsToExclude.Any(x => name.Name.StartsWith(x)))
+                {
+                    continue;
+                }
+
+                componentList.Add(item: $"{name.Name} - Version {name.Version}");
+            }
+
+            return componentList;
         }
 
         private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
         {
-            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+            if (e.Uri != null)
+            {
+                Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+            }
             e.Handled = true;
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
+            Close();
         }
     }
 }
