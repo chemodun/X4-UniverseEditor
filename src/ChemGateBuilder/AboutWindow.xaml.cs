743b054ab522424fea64a9c83e2250f3b1e5336d
Chem O'Dun <chemodun@gmail.com>
2025-02-04 23:54:47 +0200

    fix(AboutWindow): add builder icon and adjust button margin

--- src/ChemGateBuilder/AboutWindow.xaml.cs
+++ src/ChemGateBuilder/AboutWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace ChemGateBuilder
             "Windows",
             "UIAutomation",
             "DirectWriteForwarder",
+            "netstandard",
         ];
         public AboutWindow()
         {
