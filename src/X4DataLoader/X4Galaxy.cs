056a168e2ee5fda60de5517743c4e169e9bcf5e0
Chem O'Dun <chemodun@gmail.com>
2025-02-15 13:16:17 +0200

    refactor(X4Galaxy): move data validation method for X4 data folder inside

--- src/X4DataLoader/X4Galaxy.cs
+++ src/X4DataLoader/X4Galaxy.cs
@@ -15,5 +15,22 @@ namespace X4DataLoader
 
       return galaxy;
     }
+
+    public static bool ValidateDataFolder(string folderPath, out string errorMessage)
+    {
+      string subfolderPath = System.IO.Path.Combine(folderPath, "t");
+      string filePath = System.IO.Path.Combine(subfolderPath, "0001-l044.xml");
+
+      if (Directory.Exists(subfolderPath) && File.Exists(filePath) && new FileInfo(filePath).Length > 0)
+      {
+        errorMessage = string.Empty;
+        return true;
+      }
+      else
+      {
+        errorMessage = $"Error: Folder does not contain required X4 data ({folderPath})";
+        return false;
+      }
+    }
   }
 }
