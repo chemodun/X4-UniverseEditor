1bcf20dd6618ae5bfd8b0726ed3922d67976b5ae
Chem O'Dun <chemodun@gmail.com>
2025-02-15 12:29:54 +0200

    feat(GalaxyEditor): initialize project structure with App.xaml and MainWindow

---
+++ src/GalaxyEditor/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GalaxyEditor;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
