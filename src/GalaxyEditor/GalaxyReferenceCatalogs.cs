9657c2250087132b93fa7deb41884df25a3b2186
Chem O'Dun <chemodun@gmail.com>
2025-02-19 15:51:27 +0200

    feat(GalaxyReferencesHolder): add support for ClusterMusic and ClusterIcons properties, and some elements manually added (like None)

--- src/GalaxyEditor/GalaxyReferenceCatalogs.cs
+++ src/GalaxyEditor/GalaxyReferenceCatalogs.cs
@@ -1,3 +1,4 @@
+using System.Media;
 using X4DataLoader;
 
 namespace GalaxyEditor
@@ -15,8 +16,17 @@ namespace GalaxyEditor
     public bool Added { get; set; } = false;
   }
 
+  public class CatalogItemWithStringId(string id, string text)
+  {
+    public string Id { get; private set; } = id;
+    public string Text { get; private set; } = text;
+    public bool Added { get; set; } = false;
+  }
+
   public class GalaxyReferencesHolder
   {
+    public static readonly int GalaxyItemsAttributesPage = 1042;
+    public static readonly int GalaxyItemsSuffixesPage = 20403;
     private readonly Galaxy _galaxyData = new();
 
     public List<CatalogItemString> StarSystems { get; private set; } = [];
@@ -30,6 +40,8 @@ namespace GalaxyEditor
     public List<CatalogItemWithIntId> StarSuffixes { get; private set; } = [];
     public List<CatalogItemWithIntId> PlanetSuffixes { get; private set; } = [];
     public List<CatalogItemWithIntId> MoonSuffixes { get; private set; } = [];
+    public List<CatalogItemWithStringId> ClusterMusic { get; private set; } = [];
+    public List<CatalogItemString> ClusterIcons { get; private set; } = [];
 
     public GalaxyReferencesHolder(Galaxy? galaxyData = null)
     {
@@ -43,19 +55,21 @@ namespace GalaxyEditor
             StarSystems.Add(new CatalogItemString(clusterItem.System));
           }
         }
-        PlanetPopulation.Add(new CatalogItemWithIntId(10011, _galaxyData.Translation.TranslateByPage(1042, 10011)));
-        PlanetPopulation.Add(new CatalogItemWithIntId(10021, _galaxyData.Translation.TranslateByPage(1042, 10021)));
+        PlanetPopulation.Add(new CatalogItemWithIntId(10011, _galaxyData.Translation.TranslateByPage(GalaxyItemsAttributesPage, 10011)));
+        PlanetPopulation.Add(new CatalogItemWithIntId(16011, _galaxyData.Translation.TranslateByPage(GalaxyItemsAttributesPage, 16011)));
+        PlanetPopulation.Add(new CatalogItemWithIntId(10021, "Inhabited"));
         for (int textId = 11000; textId < 12000; textId++)
         {
-          string text = galaxyData.Translation.TranslateByPage(1042, textId);
+          string text = galaxyData.Translation.TranslateByPage(GalaxyItemsAttributesPage, textId);
           if (text != "")
           {
             PlanetAtmosphere.Add(new CatalogItemWithIntId(textId, text));
           }
         }
+        PlanetAtmosphere.Add(new CatalogItemWithIntId(10011, _galaxyData.Translation.TranslateByPage(GalaxyItemsAttributesPage, 10011)));
         for (int textId = 12000; textId < 13000; textId++)
         {
-          string text = galaxyData.Translation.TranslateByPage(1042, textId);
+          string text = galaxyData.Translation.TranslateByPage(GalaxyItemsAttributesPage, textId);
           if (text != "")
           {
             Environments.Add(new CatalogItemWithIntId(textId, text));
@@ -63,7 +77,7 @@ namespace GalaxyEditor
         }
         for (int textId = 13000; textId < 14000; textId++)
         {
-          string text = _galaxyData.Translation.TranslateByPage(1042, textId);
+          string text = _galaxyData.Translation.TranslateByPage(GalaxyItemsAttributesPage, textId);
           if (text != "")
           {
             StarClasses.Add(new CatalogItemWithIntId(textId, text));
@@ -71,7 +85,7 @@ namespace GalaxyEditor
         }
         for (int textId = 14000; textId < 15000; textId++)
         {
-          string text = _galaxyData.Translation.TranslateByPage(1042, textId);
+          string text = _galaxyData.Translation.TranslateByPage(GalaxyItemsAttributesPage, textId);
           if (text != "")
           {
             PlanetClasses.Add(new CatalogItemWithIntId(textId, text));
@@ -79,7 +93,7 @@ namespace GalaxyEditor
         }
         for (int textId = 15000; textId < 16000; textId++)
         {
-          string text = _galaxyData.Translation.TranslateByPage(1042, textId);
+          string text = _galaxyData.Translation.TranslateByPage(GalaxyItemsAttributesPage, textId);
           if (text != "")
           {
             PlanetGeology.Add(new CatalogItemWithIntId(textId, text));
@@ -87,15 +101,16 @@ namespace GalaxyEditor
         }
         for (int textId = 16000; textId < 17000; textId++)
         {
-          string text = _galaxyData.Translation.TranslateByPage(1042, textId);
+          string text = _galaxyData.Translation.TranslateByPage(GalaxyItemsAttributesPage, textId);
           if (text != "")
           {
             PlanetSettlements.Add(new CatalogItemWithIntId(textId, text));
           }
         }
+        PlanetSettlements.Add(new CatalogItemWithIntId(10011, _galaxyData.Translation.TranslateByPage(GalaxyItemsAttributesPage, 10011)));
         for (int textId = 1; textId < 121; textId++)
         {
-          string text = _galaxyData.Translation.TranslateByPage(20403, textId);
+          string text = _galaxyData.Translation.TranslateByPage(GalaxyItemsSuffixesPage, textId);
           if (text != "")
           {
             StarSuffixes.Add(new CatalogItemWithIntId(textId, text));
@@ -103,7 +118,7 @@ namespace GalaxyEditor
         }
         for (int textId = 101; textId < 121; textId++)
         {
-          string text = _galaxyData.Translation.TranslateByPage(20403, textId);
+          string text = _galaxyData.Translation.TranslateByPage(GalaxyItemsSuffixesPage, textId);
           if (text != "")
           {
             PlanetSuffixes.Add(new CatalogItemWithIntId(textId, text));
@@ -115,6 +130,28 @@ namespace GalaxyEditor
           if (text != "")
           {
             MoonSuffixes.Add(new CatalogItemWithIntId(textId, text));
+          }
+        }
+        foreach (X4Sound sound in galaxyData.Sounds)
+        {
+          if (
+            sound.Id.StartsWith("music_")
+            && !sound.Id.Contains("gamestart")
+            && !sound.Id.Contains("highway")
+            && !sound.Id.Contains("scenario")
+            && !sound.Id.Contains("story")
+            && !sound.Id.Contains("tutorial")
+            && !sound.Id.Contains("_hq_")
+          )
+          {
+            ClusterMusic.Add(new CatalogItemWithStringId(sound.Id, sound.Description));
+          }
+        }
+        foreach (X4Icon icon in galaxyData.Icons)
+        {
+          if (icon.Id.Contains("enc_cluster"))
+          {
+            ClusterIcons.Add(new CatalogItemString(icon.Id));
           }
         }
       }
