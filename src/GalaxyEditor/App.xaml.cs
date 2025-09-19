d0badce43f3854723d4cb193b2c058b5459491e5
Chem O'Dun <chemodun@gmail.com>
2025-09-19 13:06:58 +0300

    refactor: remove unused using directives from App.xaml.cs files

--- src/GalaxyEditor/App.xaml.cs
+++ src/GalaxyEditor/App.xaml.cs
@@ -1,16 +1,9 @@
-﻿using System.Configuration;
-using System.Data;
-using System.IO;
-using System.Text.Json;
-using System.Windows;
+﻿using System.Windows;
 using NLog;
 using Utilities.Logging;
 
 namespace GalaxyEditor
 {
-  /// <summary>
-  /// Interaction logic for App.xaml
-  /// </summary>
   public partial class App : Application
   {
     protected override void OnStartup(StartupEventArgs e)
