d511e7cb4d0c40e5ecd9afed1f1dff01acfd337e
Chem O'Dun <chemodun@gmail.com>
2025-09-22 21:45:35 +0300

    refactor(GalaxyMapViewerForGateBuilder): add GalaxyMapViewerForGateBuilder class
      src/ChemGateBuilder/GalaxyMapViewerForGateBuilder.cs: +added class to handle extra connections in the galaxy map
      src/ChemGateBuilder/GalaxyMapWindow.xaml: ~updated to use GalaxyMapViewerForGateBuilder instead of GalaxyMapViewer

---
+++ src/ChemGateBuilder/GalaxyMapViewerForGateBuilder.cs
@@ -0,0 +1,54 @@
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using Utilities.Logging;
+using X4DataLoader;
+using X4Map;
+
+namespace ChemGateBuilder
+{
+  public class GalaxyMapViewerForGateBuilder : GalaxyMapViewer
+  {
+    private List<string> _extraConnectionsNames = [];
+
+    public void Connect(
+      Galaxy galaxy,
+      Canvas galaxyCanvas,
+      double mapColorsOpacity,
+      bool editorMode = true,
+      Dictionary<string, List<ObjectInSector>>? extraObjects = null,
+      List<string>? extraConnectionsNames = null
+    )
+    {
+      base.Connect(galaxy, galaxyCanvas, mapColorsOpacity, editorMode, extraObjects);
+      _extraConnectionsNames = extraConnectionsNames ?? [];
+    }
+
+    protected override void CreateMap()
+    {
+      base.CreateMap();
+      foreach (string connectionName in _extraConnectionsNames)
+      {
+        if (connectionName == null)
+        {
+          continue;
+        }
+        List<SectorMapItem> extraGatesItems = SectorsItems.FindAll(item => item.Id == connectionName);
+        if (extraGatesItems.Count == 2)
+        {
+          GalaxyMapInterConnection galaxyMapGateConnection = new(extraGatesItems[0], extraGatesItems[1], true);
+          galaxyMapGateConnection.Create(GalaxyCanvas);
+          InterConnections.Add(galaxyMapGateConnection);
+        }
+      }
+      List<SectorMapItem> newGatesItems = SectorsItems.FindAll(item => item.Id == SectorMap.NewGateId);
+      if (newGatesItems.Count == 2)
+      {
+        GalaxyMapInterConnection galaxyMapGateConnection = new(newGatesItems[0], newGatesItems[1], true);
+        galaxyMapGateConnection.Create(GalaxyCanvas);
+        InterConnections.Add(galaxyMapGateConnection);
+      }
+    }
+  }
+}
